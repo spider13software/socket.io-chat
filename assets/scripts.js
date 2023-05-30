c2902e8344fbef9cdccd72206c9a18f770cb48a2
Spider13 <admin@spider13.net>
2023-05-30 17:08:03 +0300

    Commit

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,17 +1,50 @@
+let authLogin = null;
+let authToken = null;
+
+async function doLogin(login, password) {
+  const response = await fetch('/login', {
+    method: 'POST',
+    body: JSON.stringify({
+      login,
+      password,
+    }),
+  });
+  const responseData = await response.json();
+  return responseData;
+}
+
 $(function () {
+  $('#login-form').submit(function (event) {
+    event.preventDefault();
+
+    const login = $('#login-form input[name=login]').val();
+    const password = $('#login-form input[name=password]').val();
+
+    doLogin(login, password).then((response) => {
+      if (response.success) {
+        authLogin = login;
+        authToken = token;
+        $('#chat-form').removeClass('hide');
+        $('#login-form').addClass('hide');  
+      }
+    }).catch((error) => {
+      alert(`Error: ${error.message}`);
+    });
+  });
+
+  $('#chat-form').submit(function (event) {
+    event.preventDefault();
+
+    const message = $('#chat-form input[name=message]').val();
+    socket.emit('chat message', message);
+    $('#chat-form input[name=message]').val('');
+  });
+
   var socket = io();
 
   var messages = document.getElementById('messages');
   var form = document.getElementById('form');
   var input = document.getElementById('input');
-  
-  form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    if (input.value) {
-      socket.emit('chat message', input.value);
-      input.value = '';
-    }
-  });
   
   socket.on('chat message', function(msg) {
     var item = document.createElement('li');
--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const path = require('path');
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const { MongoClient } = require('mongodb');
 
 const port = process.env.PORT || 3000;
 
+const uri = 'mongodb://localhost/chatdb';
+const client = new MongoClient(uri);
+
 const app = express();
+app.use(express.json());
 app.set('view engine', 'ejs');
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
@@ -13,8 +18,25 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.post('/login', (req, res) => {
+  const login = req.body.login;
+  const password = req.body.password;
+  const token = `${login}${password}`;
+  res.json({
+    success: true,
+    login,
+    token,
+  });
+});
+
 const httpServer = http.Server(app);
 
 const io = new socketIo.Server(httpServer, { /* options */ });
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
 
 httpServer.listen(port);
