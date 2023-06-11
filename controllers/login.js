3a47311001d20747e02b79315f86e946eedc81be
Spider13 <admin@spider13.net>
2023-06-11 15:31:03 +0300

    Commit

--- controllers/login.js
+++ controllers/login.js
@@ -2,7 +2,12 @@ const client = require('../lib/db');
 
 module.exports = function (app) {
   app.get('/login', async (req, res) => {
-    res.render('login');
+    const flash = req.session.flash;
+    delete req.session.flash;
+
+    res.render('login', {
+      flash,
+    });
   });
 
   app.post('/login', async (req, res) => {
@@ -17,14 +22,18 @@ module.exports = function (app) {
     });
   
     if (!user) {
-      throw new Error('User not exists');
+      req.session.flash = 'User not exists';
+      res.redirect('/login');
+      return false;
     }
   
     if (user.password !== password) {
-      throw new Error('Invalid password');
+      req.session.flash = 'Invalid password';
+      res.redirect('/login');
+      return false;
     }
 
-  req.session.userId = user._id;
+    req.session.userId = user._id;
 
     res.redirect('/');
   });
--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,77 @@
 const client = require('../lib/db');
+const { ObjectId } = require('mongodb');
 const { checkAuth } = require('../lib/express');
 
 module.exports = function (app) {
-  app.get('/users', checkAuth, (req, res) => {
-    res.render('users');
+  app.get('/users', checkAuth, async (req, res) => {
+    const database = client.db('chatdb');
+    const users = database.collection('users');
+
+    const list = await users.find({}).toArray();
+
+    res.render('users', {
+      list,
+    });
+  });
+
+  app.get('/users/delete/:id', checkAuth, async (req, res) => {
+    const database = client.db('chatdb');
+    const users = database.collection('users');
+
+    const id = req.params.id;
+
+    users.deleteOne({
+      _id: new ObjectId(id),
+    });
+
+    res.redirect('/users');
+  });
+
+  app.get('/users/add', checkAuth, async (req, res) => {
+    const flash = req.session.flash;
+    delete req.session.flash;
+
+    const database = client.db('chatdb');
+    const users = database.collection('users');
+
+    res.render('edit-user', {
+      create: true,
+      flash,
+    });
+  });
+
+  app.post('/users/add', checkAuth, async (req, res) => {
+    const database = client.db('chatdb');
+    const users = database.collection('users');
+
+    const email = req.body.email;
+    const login = req.body.login;
+    const password = req.body.password;
+    const confirmPassword = req.body.confirmPassword;
+
+    if (password !== confirmPassword) {
+      req.session.flash = 'Password not confirmed';
+      res.redirect('/users/add');
+      return false;
+    }
+
+    const user = await users.findOne({
+      login,
+    });
+
+    if (user) {
+      req.session.flash = 'User already exists';
+      res.redirect('/users/add');
+      return false;
+    }
+
+    await users.insertOne({
+      email,
+      login,
+      password,
+    });
+
+    res.redirect('/users');
+
   });
 };
